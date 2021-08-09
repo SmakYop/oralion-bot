9ac10cf76c993c95e8ef8396c711d4236379d24a
SmakYop <arthurpresles@hotmail.fr>
2021-08-09 18:03:00 +0200

    add Procfile

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 

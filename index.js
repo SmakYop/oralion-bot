ac696fd6bb439a4fe339750ac8a3daa1201f0bec
SmakYop <arthurpresles@hotmail.fr>
2021-08-09 17:50:12 +0200

    bot login

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+require("dotenv").config();
+
+client.login(process.env.BOT_TOKEN);
